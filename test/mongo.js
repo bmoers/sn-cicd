c3b01f7dd68423fe0511d0dd65f4f6fb05c3de80
SNOW CICD01 <boris@moers.ch>
2020-04-22 11:22:13 +0200

    a mongo test

---
+++ test/mongo.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const Promise = require('bluebird');
+
+
+const mongoDao = require('../lib/prototype/mongo-dao').call({
+  dataStore: {
+    application: null,
+    us: null,
+    run: null,
+    step: null,
+    deployment: null,
+    test: null
+  }
+});
+
+
+return Promise.try(() => {
+
+  return mongoDao.run.find({
+    usId: "5e9834026e56ea5f40334be7"
+  });
+}).then((runList) => {
+  var maxRun = 2;
+  var sortedRun = runList.sort((a, b) => {
+    return b.ts - a.ts;
+  });
+  var length = -1 * (sortedRun.length - maxRun);
+  console.log(length)
+  var length = -1 * (sortedRun.length - maxRun);
+
+  console.log(sortedRun.map((r) => r.sequence))
+
+  if (length < 0) {
+    console.log(`\tto be deleted # ${length * -1}`);
+    var removeRun = sortedRun.slice(length);
+    console.log(removeRun.map((r) => r.sequence))
+  }
+})
