39e97cb3f5a4614176f52816160cd97208df1d96
Boris Moers <boris@moers.ch>
2021-05-13 16:55:04 +0200

    ecosystem examples

--- ecosystem-server.config.js
+++ ecosystem-server.config.js
@@ -2,7 +2,6 @@ module.exports = {
     apps: [{
         name: 'server',
         script: './server.js',
-        instances: 2,
         env: {
             NODE_ENV: 'production',
             CICD_EB_WORKER_CLUSTER_NUM: 0
