680362c3688b63972e7acf8256bc456b439a323d
Boris Moers <boris@moers.ch>
2021-05-25 00:12:31 +0200

    tests extended

--- test/job-test.js
+++ test/job-test.js
@@ -8,37 +8,65 @@ const CICD = require('../lib/cicd');
 const ExeJob = require('../lib/eb/job');
 const QueueJob = require('../lib/eb/queue-job');
 
-CICD.prototype.jobs = async function () {
+CICD.prototype.exeJobs = async function (num, background) {
 
 
-    const list = [1, 2, 3, 4, 5, 6, 7, 8, 9].map(e => {
-        return new QueueJob({ name: 'dummy', description: 'number: ' + e, background: true, exclusiveId: 1 }, { body: 'body ' + e });
-    });
+    const list1 = Promise.map(Array(num).fill(0), ((e, index) => {
+        return new ExeJob({ name: 'dummy', description: 'number: ' + e, background, exclusiveId: 1 }, { body: 'body ' + e, index });
+    }));
 
-
-    const list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9].map(e => {
-        return new QueueJob({ name: 'dummy', description: 'exclusive 2: ' + e, background: true, exclusiveId: 2 }, { body: 'body ' + e });
-    });
+    // mapSeries
+    const list2 = Promise.map(Array(num).fill(0), ((e, index) => {
+        return new ExeJob({ name: 'dummy', description: 'exclusive 2: ' + e, background, exclusiveId: 2 }, { body: 'body ' + e, index });
+    }));
 
     console.log('------------------------ wait for all');
-    const res = await Promise.all(list.concat(list2));
+
+    const out = await Promise.all([list1, list2]);
+    const res = [...out[0], ...out[1]];
 
     console.log('------------------------ done');
-    console.log(res.map(r => r.result));
-    console.log(res);
+    if (background) {
+        console.log('job ids');
+        console.log(res.map(r => r._id));
+    } else {
+        console.log('job results');
+        console.log(res.map(r => r.result));
+    }
 
-    /*
-    // this job is executed via 'exe' and pushed to the workers
-    const result = await new ExeJob({ name: 'dummy', background: false }, { body: 'body' });
-    console.log('result----------------------------------------->', result);
-    */
+};
+CICD.prototype.queueJobs = async function (num, background) {
+
+
+    const list1 = Promise.map(Array(num).fill(0), ((e, index) => {
+        return new QueueJob({ name: 'dummy', description: 'number: ' + e, background, _exclusiveId: 3 }, { body: 'body ' + e, index });
+    }));
+
+    // mapSeries
+    const list2 = Promise.map(Array(num).fill(0), ((e, index) => {
+        return new QueueJob({ name: 'dummy', description: 'exclusive 2: ' + e, background, _exclusiveId: 4 }, { body: 'body ' + e, index });
+    }));
+
+    console.log('------------------------ wait for all');
+
+    const out = await Promise.all([list1, list2]);
+    const res = [...out[0], ...out[1]];
+
+    console.log('------------------------ done');
+    if (background) {
+        console.log('job ids');
+        console.log(res.map(r => r._id));
+    } else {
+        console.log('job results');
+        console.log(res.map(r => r.result));
+    }
 };
 
 CICD.prototype.job = async function () {
 
 
 
-    const result = await new ExeJob({ name: 'dummy', background: true, description: 'dummy job', exclusiveId: 2 }, { body: 'body ' });
+    const result = await new ExeJob({ name: 'dummy', background: true, description: 'dummy job', exclusiveId: 5 }, { body: 'body ', index: 0 });
 
     // console.log('------------------------ done');
     // console.log(result);
@@ -58,8 +86,9 @@ CICD.prototype.job = async function () {
         c.self.init(c.self.WORKER);
 
         console.log('test jobs');
-        c.self.job();
-        await c.self.job();
+
+        await c.self.exeJobs(10,true);
+        await c.self.queueJobs(1,false);
         /*
          const job = await c.self.job();
          console.log(job);
